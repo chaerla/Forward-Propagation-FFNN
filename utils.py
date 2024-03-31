106a00100c59ab8d066471aa1d806068b4305d4c
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-03-31 19:46:24 +0700

    feat: get random pale color

--- utils.py
+++ utils.py
@@ -1,7 +1,22 @@
 import numpy as np
+import random
+
 
 def relu(x):
     return np.maximum(x, 0)
 
+
 def sigmoid(x):
     return 1.0/(1.0 + np.exp(-x))
+
+
+def get_random_pale_color():
+    """
+    Generates a random pale color in hexadecimal format.
+
+    :return: A random pale color.
+    """
+    r = random.randint(200, 255)
+    g = random.randint(200, 255)
+    b = random.randint(200, 255)
+    return '#%02x%02x%02x' % (r, g, b)
