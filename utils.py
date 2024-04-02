206d4226eb0457302ce05f3e6d51d4d3c7dd1c43
Alexander Jason <alexanderjason526@gmail.com>
2024-04-02 10:45:12 +0700

    feat: add activation function linear and softmax

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 import numpy as np
 import random
 
+def linear(x):
+    return x
 
 def relu(x):
     return np.maximum(x, 0)
 
-
 def sigmoid(x):
     return 1.0/(1.0 + np.exp(-x))
 
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
 
 def get_random_pale_color():
     """
